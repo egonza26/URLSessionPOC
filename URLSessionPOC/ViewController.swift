b9775f38faf3d03da2d897235b9c3e51b28f05b0
Ernesto Gonzalez <ernesto.gonzalez@gorillalogic.com>
2019-05-08 16:17:30 -0600

    Initial Commit

---
+++ URLSessionPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URLSessionPOC
+//
+//  Created by Ernesto Gonzalez on 5/8/19.
+//  Copyright © 2019 Ernesto Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
